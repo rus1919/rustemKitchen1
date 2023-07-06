243cc49d17fa55bf92fc700b18e87a2b8142b879
RMS <rms@citydom.ru>
2023-07-06 09:34:06 +0300

    3

--- db_func.py
+++ db_func.py
@@ -1,26 +1,90 @@
 import datetime
 import sqlite3
 from config import db_path
-def new_user(tlgm_id,user_name):
+
+
+def new_user(tlgm_id, user_name):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     sql = f'select * from users where tlgm_id={tlgm_id}'
     cursor.execute(sql)
-    if cursor.fetchone()==None:
+    if cursor.fetchone() == None:
         sql = 'insert into users (tlgm_id,user_name,date_reg) values (?,?,?)'
         date_reg = str(datetime.datetime.now().replace(microsecond=0))
-        cursor.execute(sql,(tlgm_id,user_name,date_reg))
+        cursor.execute(sql, (tlgm_id, user_name, date_reg))
     conn.commit()
-def calc_filler(tlgm_id,type_kitchen=None,column=None,value=None):
+
+
+def calc_filler(tlgm_id, column, value):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     sql = f"select * from orders where tlgm_id={tlgm_id}"
     cursor.execute(sql)
-    if cursor.fetchone()==None:
+    if cursor.fetchone() == None:
         sql = "insert into orders (tlgm_id,type_kitchen) values (?,?)"
-        cursor.execute(sql,(tlgm_id,type_kitchen))
+        cursor.execute(sql, (tlgm_id, value))
     else:
-        if column!=None:
+        if column == 'type_kitchen':
             sql = f"update orders set {column}='{value}' where tlgm_id={tlgm_id}"
-            cursor.execute(sql)
+        else:
+            sql = f"update orders set {column}='{value}' where tlgm_id={tlgm_id}"
+        cursor.execute(sql)
+
     conn.commit()
+
+
+def get_column_value(tlgm_id, column):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    sql = f"select {column} from orders where tlgm_id={tlgm_id}"
+    cursor.execute(sql)
+    return cursor.fetchone()[0]
+
+
+def get_pict(duhovka=None, vitag=None, micro=None, holod=None, type_kitchen=None):
+    res = f'shag2/duhovka/duh-{duhovka}.webp'
+    if vitag != None:
+        res = f'shag2/vitag/duh-{duhovka}_vitag-{vitag}.webp'
+        if micro != None:
+            res = f'shag2/micro/duh-{duhovka}_vitag-{vitag}_micro-{micro}.webp'
+            if holod != None:
+                res = f'shag2/holod/duh-{duhovka}_vitag-{vitag}_micro-{micro}_holod-{holod}.webp'
+    if type_kitchen == 'premaya':
+        res = 'shag3/pr/shkaf_zastavka.webp'
+    if type_kitchen == 'uglovaya_levaya':
+        res = 'shag3/ugl/zastavk.webp'
+    if type_kitchen == 'uglovaya_pravaya':
+        res = 'shag3/ugl/zastavk.webp'
+    if type_kitchen == 'p_obraznaya':
+        res = 'shag3/pobraz/zastav.webp'
+
+    return res
+
+
+def get_type_verh_skaf(type_kitchen):
+    d = {
+        'uglovaya_levaya': {'pict1_1': {'path':'/static/img/calc/shag3/ugl/bez_verh.webp','text':'Без верха'},
+                            'pict1_2': {'path':'/static/img/calc/shag3/ugl/verh a.webp','text':'Верх A'},
+                            'pict2_1': {'path':'/static/img/calc/shag3/ugl/verh_a_b.webp','text':'Верх А-B'},
+                            'pict2_2': {'path':'/static/img/calc/shag3/ugl/verh_b.webp','text':'Верх B'}
+                            },
+        'uglovaya_pravaya': {'pict1_1': {'path':'/static/img/calc/shag3/ugl/bez_verh.webp','text':'Без верха'},
+                             'pict1_2': {'path':'/static/img/calc/shag3/ugl/verh a.webp','text':'Верх A'},
+                             'pict2_1': {'path':'/static/img/calc/shag3/ugl/verh_a_b.webp','text':'Верх A-B'},
+                             'pict2_2': {'path':'/static/img/calc/shag3/ugl/verh_b.webp','text':'Верх B'}
+                             },
+        'p_obraznaya': {'pict1_1': {'path':'/static/img/calc/shag3/pobraz/bez_verh.webp','text':'Без верха'},
+                        'pict1_2': {'path':'/static/img/calc/shag3/pobraz/verh_a.webp','text':'Верх A'},
+                        'pict2_1': {'path':'/static/img/calc/shag3/pobraz/verh_a_b.webp','text':'Верх A-B'},
+                        'pict2_2': {'path':'/static/img/calc/shag3/pobraz/verh_a_b_c.webp','text':'Верх A-B-C'},
+                        'pict3_1': {'path':'/static/img/calc/shag3/pobraz/verh_b.webp','text':'Верх B'},
+                        'pict3_3': {'path':'/static/img/calc/shag3/pobraz/verh_b_c.webp','text':'Верх B-C'},
+                        'pict4_1': {'path':'/static/img/calc/shag3/pobraz/verh_c.webp','text':'Верх C'}
+                        },
+        'premaya': {'pict1_1': {'path':'/static/img/calc/shag3/pr/s_antresol.webp','text':'С антресолью'},
+                    'pict1_2': {'path':'/static/img/calc/shag3/pr/s_glubokoi antresol.webp','text':'С глубокой антресолью'},
+                    'pict2_1': {'path':'/static/img/calc/shag3/pr/standart.webp','text':'Стандарт'},
+                    'pict2_2': {'path':'/static/img/calc/shag3/pr/visokii.webp','text':'Высокий'}
+                    },
+    }
+    return d[type_kitchen]
--- main.py
+++ main.py
@@ -1,39 +1,128 @@
-import asyncio
 import random
-import sys
-import traceback
-
-#import gevent
-#from gevent.pywsgi import WSGIServer
-
-from flask import Flask, request, render_template, send_file, flash, redirect
-import config
-import db_func
+from flask import Flask, request, render_template
+from db_func import calc_filler, get_column_value, get_pict, get_type_verh_skaf
 from config import path
 
-
-
 app = Flask(__name__)
+
+
 @app.route('/favicon.ico')
 def favicon():
     return 'True'
+
+
 @app.route('/calc/<tlgm_id>', methods=['GET', 'POST'])
 def main(tlgm_id):
     rq = request.form
-    rand = random.randint(1,44)
-    if request.method=='POST':
+    rand = random.randint(1, 44)
+    if request.method == 'POST':
         print(rq)
         if 'step1' in rq.keys():
             type_kitchen = rq['step1']
-            db_func.calc_filler(tlgm_id=tlgm_id,type_kitchen=type_kitchen)
-            return render_template('main.html', step=2, type_kitchen =type_kitchen ,rand = rand)
+            calc_filler(tlgm_id=tlgm_id, column='type_kitchen', value=type_kitchen)
+            posudomoika = 3
+            oborud = 'Посудомойка'
+            header_text = 'Выберите технику для кухни'
+            return render_template('main.html', step=2.0, type_kitchen=type_kitchen, rand=rand,
+                                   posudomoika=posudomoika, oborud=oborud, header_text=header_text)
+        if 'posudomoika' in rq.keys():
+            posudomoika = 0
+            if rq['posudomoika'] == 'da':
+                posudomoika = 1
+            oborud = 'Посудомойка'
+            header_text = 'Выберите технику для кухни'
+            return render_template('main.html', step=2.0, posudomoika=posudomoika, rand=rand, oborud=oborud,
+                                   header_text=header_text)
+        if 'duhovka' in rq.keys() and 'save' not in rq.keys():
+            duhovka = rq['duhovka']
+            oborud = 'Духовка'
+            pict = get_pict(duhovka=duhovka)
+            header_text = 'Выберите технику для кухни'
+            return render_template('main.html', step=2.1, duhovka=duhovka, rand=rand, oborud=oborud, pict=pict,
+                                   header_text=header_text)
+        if 'vitag' in rq.keys() and 'save' not in rq.keys():
+            vitag = rq['vitag']
+            oborud = 'Вытяжка'
+            duhovka = get_column_value(tlgm_id=tlgm_id, column='duhovka')
+            pict = get_pict(duhovka=duhovka, vitag=vitag)
+            header_text = 'Выберите технику для кухни'
+            return render_template('main.html', step=2.2, duhovka=duhovka, vitag=vitag, rand=rand, oborud=oborud,
+                                   pict=pict, header_text=header_text)
+        if 'micro' in rq.keys() and 'save' not in rq.keys():
+            micro = rq['micro']
+            oborud = 'Микроволновка'
+            duhovka = get_column_value(tlgm_id=tlgm_id, column='duhovka')
+            vitag = get_column_value(tlgm_id=tlgm_id, column='vitag')
+            pict = get_pict(duhovka=duhovka, vitag=vitag, micro=micro)
+            header_text = 'Выберите технику для кухни'
+            return render_template('main.html', step=2.3, duhovka=duhovka, vitag=vitag, micro=micro, rand=rand,
+                                   oborud=oborud, pict=pict, header_text=header_text)
+        if 'holod' in rq.keys() and 'save' not in rq.keys():
+            holod = rq['holod']
+            oborud = 'Холодильник'
+            micro = get_column_value(tlgm_id=tlgm_id, column='micro')
+            duhovka = get_column_value(tlgm_id=tlgm_id, column='duhovka')
+            vitag = get_column_value(tlgm_id=tlgm_id, column='vitag')
+            header_text = 'Выберите технику для кухни'
+            pict = get_pict(duhovka=duhovka, vitag=vitag, micro=micro, holod=holod)
 
-    return render_template('main.html', step=1, rand = rand)
+            return render_template('main.html', step=2.4, duhovka=duhovka, vitag=vitag, holod=holod, miсro=micro,
+                                   rand=rand, oborud=oborud, pict=pict, header_text=header_text)
+
+        if 'save' in rq.keys():
+            step = float(rq['save'])
+            if step == 2.0:
+                oborud = 'Духовка'
+                calc_filler(tlgm_id=tlgm_id, column='posud', value=rq['posud'])
+                pict = 'shag2/posud/posud-da.webp'
+                header_text = 'Выберите технику для кухни'
+                return render_template('main.html', step=2.1, rand=rand, oborud=oborud, pict=pict,
+                                       header_text=header_text)
+            if step == 2.1:
+                oborud = 'Вытяжка'
+                calc_filler(tlgm_id=tlgm_id, column='duhovka', value=rq['duhovka'])
+                duhovka = get_column_value(tlgm_id=tlgm_id, column='duhovka')
+                pict = get_pict(duhovka=duhovka)
+                header_text = 'Выберите технику для кухни'
+                return render_template('main.html', step=2.2, rand=rand, oborud=oborud, duhovka=duhovka, pict=pict,
+                                       header_text=header_text)
+            if step == 2.2:
+                header_text = 'Выберите технику для кухни'
+                oborud = 'Микроволновка'
+                calc_filler(tlgm_id=tlgm_id, column='vitag', value=rq['vitag'])
+                duhovka = get_column_value(tlgm_id=tlgm_id, column='duhovka')
+                vitag = get_column_value(tlgm_id=tlgm_id, column='vitag')
+                pict = get_pict(duhovka=duhovka, vitag=vitag)
+                return render_template('main.html', step=2.3, rand=rand, oborud=oborud, duhovka=duhovka, vitag=vitag,
+                                       pict=pict, header_text=header_text)
+            if step == 2.3:
+                oborud = 'Холодильник'
+                calc_filler(tlgm_id=tlgm_id, column='micro', value=rq['micro'])
+                duhovka = get_column_value(tlgm_id=tlgm_id, column='duhovka')
+                vitag = get_column_value(tlgm_id=tlgm_id, column='vitag')
+                micro = get_column_value(tlgm_id=tlgm_id, column='micro')
+                pict = get_pict(duhovka=duhovka, vitag=vitag, micro=micro)
+                header_text = 'Выберите технику для кухни'
+                return render_template('main.html', step=2.4, rand=rand, oborud=oborud, duhovka=duhovka, vitag=vitag,
+                                       pict=pict, header_text=header_text)
+            if step == 2.4:
+                header_text = 'Выберите вид верхних шкафов'
+                calc_filler(tlgm_id=tlgm_id, column='holod', value=rq['holod'])
+                type_kitchen = get_column_value(tlgm_id=tlgm_id, column='type_kitchen')
+                pict = get_pict(type_kitchen=type_kitchen)
+                sub_pict = get_type_verh_skaf(type_kitchen)
+                print(type_kitchen)
+                return render_template('main.html', step=3.0, rand=rand, pict=pict, header_text=header_text,
+                                       type_kitchen=type_kitchen,sub_pict=sub_pict)
+        if 'back' in rq.keys():
+            return render_template('main.html', step=float(rq['back']))
+    return render_template('main.html', step=1, rand=rand)
+
 
 ############################
 
 
-print(path,'path')
+print(path, 'path')
 cert = path + 'cert/incyprus_pro.crt'
 key = path + 'cert/incyprus_pro.key'
 if __name__ == '__main__':
